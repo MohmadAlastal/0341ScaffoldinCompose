ef8931b2d6d5a058ced0085a8f19405c9b5e4ef8
Mohammed Al-Astal <104036868+MohmadAlastal@users.noreply.github.com>
2023-07-24 07:58:39 -0700

    0341 Scaffold in Compose

---
+++ app/src/main/java/com/example/a0341scaffoldincompose/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.a0341scaffoldincompose
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.AlertDialogDefaults.containerColor
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a0341scaffoldincompose.Scaffold.scaffold
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            scaffold()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/a0341scaffoldincompose/Scaffold/scaffold.kt
@@ -0,0 +1,36 @@
+package com.example.a0341scaffoldincompose.Scaffold
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun scaffold(){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Test") },
+                colors = TopAppBarDefaults.largeTopAppBarColors(Color.Green),
+
+                )
+        },
+        content = {
+                innerPadding ->   Text(
+            text = "Body content",
+            modifier = Modifier
+                .padding(innerPadding)//.fillMaxSize().wrapContentSize()
+
+
+        )
+        }
+    )
+}
