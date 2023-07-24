ebda440a30d7a6c046ed33e73cd60d9722fb3212
Mohammed Al-Astal <104036868+MohmadAlastal@users.noreply.github.com>
2023-07-24 14:05:46 -0700

    a

--- app/src/main/java/com/example/a0341scaffoldincompose/Scaffold/scaffold.kt
+++ app/src/main/java/com/example/a0341scaffoldincompose/Scaffold/scaffold.kt
@@ -2,6 +2,7 @@ package com.example.a0341scaffoldincompose.Scaffold
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -27,10 +28,16 @@ fun scaffold(){
                 innerPadding ->   Text(
             text = "Body content",
             modifier = Modifier
-                .padding(innerPadding)//.fillMaxSize().wrapContentSize()
+                .padding(innerPadding) //.fillMaxSize().wrapContentSize()
 
 
         )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+
+            }
         }
+
     )
 }
